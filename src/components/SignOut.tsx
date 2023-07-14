889d5400c4b2f262a7c6b4f04c20add7cb2ff3cc
hsavzyan <hsavzyan@gmail.com>
2023-07-14 21:58:03 +0400

    SignOut

---
+++ src/components/SignOut.tsx
@@ -0,0 +1,31 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+
+// Define the SignOut component
+const SignOut = () => {
+  // Define the handleSignOut function
+  // This function will be called when the user clicks the sign out button
+  const handleSignOut = () => {
+    // Call the signOut function and use the then method to handle the promise it returns
+    signOut(auth)
+      .then(() => {
+        // The user has been signed out
+      })
+      .catch((error) => {
+        // If there's an error, log it to the console
+        console.error("Error signing out: ", error);
+      });
+  };
+
+  // Render the sign out button
+  return (
+    <button
+      onClick={handleSignOut}
+      className="px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
+    >
+      Sign Out
+    </button>
+  );
+};
+
+export default SignOut;
