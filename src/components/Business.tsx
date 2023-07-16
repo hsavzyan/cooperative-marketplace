766eeda82b3e156354fe39aee867375ab68db691
hsavzyan <hsavzyan@gmail.com>
2023-07-16 15:05:29 +0400

    style  adjustments

--- src/components/Business.tsx
+++ src/components/Business.tsx
@@ -11,6 +11,7 @@ export const Business: React.FC<Props> = ({ business }) => {
   const [reviews, setReviews] = useState<ReviewType[]>(business.reviews);
   const [author, setAuthor] = useState("");
   const [content, setContent] = useState("");
+  const [showAllReviews, setShowAllReviews] = useState(false);
 
   const leaveReview = (event: React.FormEvent) => {
     event.preventDefault();
@@ -27,6 +28,8 @@ export const Business: React.FC<Props> = ({ business }) => {
     setContent("");
   };
 
+  const displayedReviews = showAllReviews ? reviews : reviews.slice(0, 2);
+
   return (
     <div className="border border-secondary p-4 rounded-md mb-4 shadow-md">
       <h1 className="font-semibold text-2xl mb-2">{business.name}</h1>
@@ -41,10 +44,18 @@ export const Business: React.FC<Props> = ({ business }) => {
 
       <h2 className="font-semibold text-xl mb-2">Reviews</h2>
       <div className="space-y-4">
-        {reviews.map((review) => (
+        {displayedReviews.map((review) => (
           <Review key={review.id} review={review} />
         ))}
       </div>
+      {!showAllReviews && reviews.length > 2 && (
+        <button
+          onClick={() => setShowAllReviews(true)}
+          className="w-full py-2 mt-4 bg-accent text-light rounded-md hover:bg-accent-dark"
+        >
+          Show more reviews
+        </button>
+      )}
 
       <h2 className="font-semibold text-xl mb-2">Leave a Review</h2>
       <form onSubmit={leaveReview} className="space-y-2">
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -13,12 +13,14 @@ export const Product: React.FC<Props> = ({ product }) => {
   };
 
   return (
-    <div className="border border-secondary p-4 rounded-md shadow-md">
-      <h2 className="font-semibold text-lg">{product.name}</h2>
-      <p>${product.price.toFixed(2)}</p>
+    <div className="flex flex-col justify-between border border-secondary p-4 rounded-md shadow-md h-full">
+      <div>
+        <h2 className="font-semibold text-lg">{product.name}</h2>
+        <p>${product.price.toFixed(2)}</p>
+      </div>
       <button
         onClick={handleAddToCartClick}
-        className="mt-2 bg-accent text-light px-4 py-2 rounded-md hover:bg-accent-dark"
+        className="mt-2 bg-accent text-light px-2 sm:px-4 py-2 rounded-md hover:bg-accent-dark self-end w-full sm:w-auto"
       >
         Add to Cart
       </button>
